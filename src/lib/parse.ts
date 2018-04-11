7498ad415c63c3eabc7fa4b9c9e3c670faddccc7
you21979 <you2197901@gmail.com>
2018-04-11 15:13:54 +0900

    fix

--- src/lib/parse.ts
+++ src/lib/parse.ts
@@ -28,9 +28,10 @@ export const parser = (fields: Array<string>): NodeInfo => {
     }
     const host: string = fields[0]
     const result: NodeInfo = fields.slice(1).reduce((r: NodeInfo, field: string)=> {
-        const protocol_short: string = field.slice(0, 1)
-        const port: number = parseInt(field.slice(1)) || port_default[field.slice(0, 1)]
-        r[protocol[protocol_short]] = port
+        const protocol_short: string = field.slice(0, 1) // 1 char
+        const option_value: string = field.slice(1)
+        const value: number = option_value !== '' ? parseInt(option_value) : port_default[protocol_short]
+        r[protocol[protocol_short]] = value
         return r
     }, {host:host, ssl:null, tcp:null, pruning:null, http:null, https:null})
     return result
